3932ed8dbf61fcbd178950644fd22111db2858ff
canSir21 <alex.mwaura@moringaschool.com>
2022-09-26 10:39:37 +0300

    commit tree 7a259e5ab4bf8e49567220d5fb488493dfa0bda0

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -4,11 +4,16 @@ class PlantsController < ApplicationController
         render json: plants
     end
     def create
-        plant = Plant.create(params.permit(:name, :price, :image))
+        plant = Plant.create(permit_args)
         render json: plant, status: 201
     end
     def show
         plant = Plant.find_by(id: params[:id])
         render json: plant
+    end
+
+    private
+    def permit_args
+        params.permit(:name, :price, :image)
     end
 end
